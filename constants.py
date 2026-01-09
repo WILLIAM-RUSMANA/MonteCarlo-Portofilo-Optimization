6fbdd99422a60d0c186af68f58ac458faf0ce2db
WILLIAM-RUSMANA <93782439+WILLIAM-RUSMANA@users.noreply.github.com>
2026-01-09 07:50:21 +0700

    main

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # risk free rate AAA bond yield
-RFR = 0.0468  # Nov 2025 rate
+RFR = 0.0554  # May 2025 rate (highest) in the following year
 CSV_FILE = "data/stocks_close_2013_2025.csv"
 CSV_BACKTEST_2025 = "data/stocks_close_2025_2026.csv"
 NUM_SIMULATIONS = 10_000  # default
